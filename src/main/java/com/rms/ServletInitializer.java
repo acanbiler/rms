9d0d5f66f6a625e25c64ffeb940b59a520d302c4
Abdullah <acanbiler@gmail.com>
2021-11-28 00:01:02 +0300

    directory change.

--- src/main/java/com/rms/ServletInitializer.java
+++ src/main/java/com/rms/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.ozu.rms;
+package com.rms;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
