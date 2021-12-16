be05bdf1ca28fd6564f7f8dca636bfe34acb31d1
Abdullah <acanbiler@gmail.com>
2021-12-17 00:33:15 +0300

    Ingredient add function.

--- src/main/java/com/rms/model/entity/Ingredient.java
+++ src/main/java/com/rms/model/entity/Ingredient.java
@@ -3,13 +3,14 @@ package com.rms.model.entity;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.io.Serializable;
 
 @Data
 @Entity
 @Table(name = "ingredient", schema = "public")
-public class Ingredient {
+public class Ingredient implements Serializable {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
